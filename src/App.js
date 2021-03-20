3dae1a44c0ea3b523af44b05c5303f4a1a71241b
Lidor Tevet <lidorte@gmail.com>
2021-03-20 14:58:52 +0200

    section 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.components.jsx";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page!</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/hats" component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
