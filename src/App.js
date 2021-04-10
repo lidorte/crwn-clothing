b939d936ae4eb6f59464b659ea9ccd60d65d6908
unknown <lidorte@gmail.com>
2021-04-10 17:25:42 +0300

    Section 6 leason 81

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.components.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
